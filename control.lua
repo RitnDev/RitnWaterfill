ea83ecaab4f505a2ffa68e38484b350efd7ec3a4
RitnDev <ritn@hotmail.fr>
2024-05-18 16:43:46 +0200

    1.4.4 - optimisation control.lua

--- control.lua
+++ control.lua
@@ -4,6 +4,4 @@ if not global.players then global.players = {} end
 -- Activation de gvv s'il est présent
 if script.active_mods["gvv"] then require(ritnlib.defines.gvv)() end
 -- Chargement de l'event listener :
-local event_listener = require(ritnlib.defines.event)
--- envoie des modules à l'event listener :
-event_listener.add_libraries(require("core/modules"))
+local event_listener = require(ritnlib.defines.event).add_libraries(require("core/modules"))
